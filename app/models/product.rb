6f460e7219569d568d27b87e85a306e22b820194
Alanah <Lana383@yahoo.com>
2013-07-26 16:58:54 -0400

    depot pg.84

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,17 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+
+# presence: true tells the validator to check that each of the named fields is present
+# and its contents are not empty
+  validates :title, :description, :image_url, presence: true
+# validates that the price is a valid, positive number
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+# ensure that no other row in the products table has the same title
+  validates :title, uniqueness: true
+# checks that the URL ends with one of the following: .gif, .jpg, or .png.
+  validates :image_url, allow_blank: true, format: {
+  	with:   %r{\.(gif|jpg|png)$}i,
+  	message: 'must be a URL for GIF, JPG or PNG image.' 
+  }
+
 end
